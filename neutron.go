bfb806fa67b488f829f6bb7e9fb49e1253347193
emersion <contact@emersion.fr>
2016-03-14 21:42:47 +0100

    Fallback to index file

--- neutron.go
+++ neutron.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"io/ioutil"
+
 	"gopkg.in/macaron.v1"
 
 	"github.com/emersion/neutron/router/api"
 )
 
 func main() {
+	publicDir := "public/build"
+	indexFile := "app.html"
+
 	m := macaron.Classic()
 	m.Use(macaron.Renderer())
 
+	// API
 	m.Group("/api", func() {
 		api.RegisterRoutes(m)
 	})
 
-	m.Use(macaron.Static("public/build", macaron.StaticOptions{
-		IndexFile: "app.html",
+	// Serve static files
+	m.Use(macaron.Static(publicDir, macaron.StaticOptions{
+		IndexFile: indexFile,
 		SkipLogging: true,
 	}))
+
+	// Fallback to index file
+	m.NotFound(func(ctx *macaron.Context) {
+
+		ctx.ServeFileContent(publicDir + "/" + indexFile)
+	})
+
+	m.NotFound(func(ctx *macaron.Context) {
+		data, err := ioutil.ReadFile(publicDir + "/" + indexFile)
+		if err != nil {
+			ctx.PlainText(404, []byte("page not found"))
+			return
+		}
+
+		ctx.Resp.Header().Set("Content-Type", "text/html")
+		ctx.Resp.Write(data)
+	})
 
 	m.Run()
 }
