476e69c6807e9de5a390319249a519517a73fe9b
emersion <contact@emersion.fr>
2016-03-15 18:11:33 +0100

    Adds Message struct, adds conversations list & count support to memory backend

--- backend/memory/conversations.go
+++ backend/memory/conversations.go
@@ -5,19 +5,43 @@ import (
 )
 
 func (b *Backend) ListConversations(user, label string, limit, page int) (convs []*backend.Conversation, total int, err error) {
-	// TODO: limit, page support	
+	allConvs := b.data[user].conversations
+	total = len(allConvs)
 
-	convs = b.data[user].conversations
+	from := limit * page
+	to := limit * (page + 1)
+	if from < 0 {
+		from = 0
+	}
+	if to > total {
+		to = total
+	}
 
-	total = len(convs)
-
+	convs = allConvs[from:to]
 	return
 }
 
 func (b *Backend) CountConversations(user string) (counts []*backend.ConversationsCount, err error) {
-	//convs := b.data[user].conversations
+	convs := b.data[user].conversations
 
-	// TODO
+	indexes := map[string]int{}
+
+	for _, c := range convs {
+		for _, label := range c.LabelIDs {
+			var count *backend.ConversationsCount
+			if i, ok := indexes[label]; ok {
+				count = counts[i]
+			} else {
+				indexes[label] = len(counts)
+				count = &backend.ConversationsCount{ LabelID: label }
+			}
+
+			count.Total++
+			if c.NumUnread > 0 {
+				count.Unread++
+			}
+		}
+	}
 
 	return
 }
---
+++ backend/messages.go
@@ -0,0 +1,32 @@
+package backend
+
+type Message struct {
+	ID string
+	Order int
+	ConversationID string
+	Subject string
+	IsRead int
+	Type int
+	SenderAddress string
+	SenderName string
+	Sender *Email
+	ToList []*Email
+	CCList []*Email
+	BCCList []*Email
+	Time int
+	Size int
+	NumAttachments int
+	IsEncrypted int
+	ExpirationTime int
+	IsReplied int
+	IsRepliedAll int
+	IsForwarded int
+	AddressID string
+	Body string
+	Header string
+	ReplyTo *Email
+	Attachments []interface{} // TODO
+	Starred int
+	Location int
+	LabelIDs []string
+}
