0110d837ed458c71669368dd652456c5a885588b
emersion <contact@emersion.fr>
2016-03-19 11:48:06 +0100

    Splits backend interface

--- backend/backend.go
+++ backend/backend.go
@@ -3,6 +3,12 @@ package backend
 
 // A backend takes care of storing all mailbox data.
 type Backend interface {
+	ContactsBackend
+	LabelsBackend
+	ConversationsBackend
+	SendBackend
+	DomainsBackend
+
 	// Check if a username is available.
 	IsUsernameAvailable(username string) (bool, error)
 	// Get a user.
@@ -22,12 +28,10 @@ type Backend interface {
 
 	// Get a public key for a user.
 	GetPublicKey(email string) (string, error)
+}
 
-	// List all domains.
-	ListDomains() ([]*Domain, error)
-	// Get the domain which has the specified name.
-	GetDomainByName(name string) (*Domain, error)
-
+// Stores contacts data.
+type ContactsBackend interface {
 	// List all user's contacts.
 	ListContacts(user string) ([]*Contact, error)
 	// Insert a new contact.
@@ -38,7 +42,10 @@ type Backend interface {
 	DeleteContact(user, id string) error
 	// Delete all contacts of a specific user.
 	DeleteAllContacts(user string) error
+}
 
+// Stores labels data.
+type LabelsBackend interface {
 	// List all user's labels.
 	ListLabels(user string) ([]*Label, error)
 	// Insert a new label.
@@ -47,13 +54,14 @@ type Backend interface {
 	UpdateLabel(user string, update *LabelUpdate) (*Label, error)
 	// Delete a label.
 	DeleteLabel(user, id string) error
+}
 
+// Stores messages data.
+type MessagesBackend interface {
 	// Get a message.
 	GetMessage(user, id string) (*Message, error)
 	// List all user's messages. A message filter can be provided.
 	ListMessages(user string, filter *MessagesFilter) ([]*Message, int, error)
-	// List all messages belonging to a conversation.
-	ListConversationMessages(user, id string) (msgs []*Message, err error)
 	// Count all user's messages by label.
 	CountMessages(user string) ([]*MessagesCount, error)
 	// Insert a new message.
@@ -62,8 +70,14 @@ type Backend interface {
 	UpdateMessage(user string, update *MessageUpdate) (*Message, error)
 	// Permanently delete a message.
 	DeleteMessage(user, id string) error
-	// Send a message to an e-mail address.
-	SendMessagePackage(user string, pkg *MessagePackage) error
+}
+
+// Stores conversations data.
+type ConversationsBackend interface {
+	MessagesBackend
+
+	// List all messages belonging to a conversation.
+	ListConversationMessages(user, id string) (msgs []*Message, err error)
 
 	// Get a specific conversation.
 	GetConversation(user, id string) (conv *Conversation, err error)
@@ -73,4 +87,18 @@ type Backend interface {
 	CountConversations(user string) ([]*MessagesCount, error)
 	// Permanently delete a conversation.
 	DeleteConversation(user, id string) error
+}
+
+// Sends messages to email addresses.
+type SendBackend interface {
+	// Send a message to an e-mail address.
+	SendMessagePackage(user string, pkg *MessagePackage) error
+}
+
+// Stores domains data.
+type DomainsBackend interface {
+	// List all domains.
+	ListDomains() ([]*Domain, error)
+	// Get the domain which has the specified name.
+	GetDomainByName(name string) (*Domain, error)
 }
