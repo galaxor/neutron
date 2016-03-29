226d3385e6852b7d7e2fe25fb469706babb679f5
emersion <contact@emersion.fr>
2016-03-29 15:47:46 +0200

    Moves UpdateKeypair() from UsersBackend to KeysBackend

--- backend/keys.go
+++ backend/keys.go
@@ -3,4 +3,6 @@ package backend
 type KeysBackend interface {
 	// Get a public key for a user.
 	GetPublicKey(email string) (string, error)
+	// Update a user's private key.
+	UpdateKeypair(id, password string, keypair *Keypair) error
 }
